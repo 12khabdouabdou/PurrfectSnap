ebed0aae150ec632c02a5b2ed29c0f017c787f7c
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-23 20:35:35 +0530

    Update ScriptingRootSection.kt

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/pages/scripting/ScriptingRootSection.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/pages/scripting/ScriptingRootSection.kt
@@ -340,7 +340,7 @@ class ScriptingRootSection : Routes.Route() {
         if (tab == 1) {
             Column(verticalArrangement = Arrangement.spacedBy(8.dp), horizontalAlignment = Alignment.End) {
                 ExtendedFloatingActionButton(
-                    onClick = { Routes.manageScriptRepos.open() },
+                    onClick = { routes.manageScriptRepos.open() },
                     icon = { Icon(Icons.Default.Public, contentDescription = null) },
                     text = { Text("Manage Repos") }
                 )
