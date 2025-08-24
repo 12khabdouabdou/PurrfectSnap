c5924b9dfebf8713a5cb5993e41b268bb79162ca
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-25 02:31:34 +0530

    Create DnsOverHttpsOkHttp.kt

---
+++ manager/src/main/kotlin/me/rhunk/snapenhance/manager/data/DnsOverHttpsOkHttp.kt
@@ -0,0 +1,23 @@
+package me.rhunk.snapenhance.manager.data
+
+import okhttp3.Dns
+import okhttp3.OkHttpClient
+import okhttp3.dnsoverhttps.DnsOverHttps
+import java.net.InetAddress
+import java.net.UnknownHostException
+
+// Cloudflare DNS-over-HTTPS endpoint
+private const val CLOUDFLARE_DOH_URL = "https://cloudflare-dns.com/dns-query"
+
+private val bootstrapDns = Dns.SYSTEM
+
+val dohOkHttp: OkHttpClient by lazy {
+    OkHttpClient.Builder()
+        .dns(
+            DnsOverHttps.Builder()
+                .client(OkHttpClient())
+                .url(CLOUDFLARE_DOH_URL.toHttpUrl())
+                .bootstrapDnsHosts(bootstrapDns.lookup("1.1.1.1"))
+                .build()
+        ).build()
+}
