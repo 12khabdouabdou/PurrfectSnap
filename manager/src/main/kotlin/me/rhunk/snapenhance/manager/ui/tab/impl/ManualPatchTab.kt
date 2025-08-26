795b1e35776b1d19ffeba74a0ad1a103d0c1cffb
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-26 16:02:06 +0530

    Update ManualPatchTab.kt

--- manager/src/main/kotlin/me/rhunk/snapenhance/manager/ui/tab/impl/ManualPatchTab.kt
+++ manager/src/main/kotlin/me/rhunk/snapenhance/manager/ui/tab/impl/ManualPatchTab.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.OpenInNew
+import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
