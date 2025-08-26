9501cd7fa891f954a45de51c730117983cec0712
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-27 00:35:14 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,6 +142,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
 
 afterEvaluate {
