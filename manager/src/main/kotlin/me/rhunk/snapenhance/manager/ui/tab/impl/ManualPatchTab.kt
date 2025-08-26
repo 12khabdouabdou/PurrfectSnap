e9e02b6e836e17982e3f85eb99d8e5bccd6ff584
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-26 14:38:31 +0530

    Update ManualPatchTab.kt

--- manager/src/main/kotlin/me/rhunk/snapenhance/manager/ui/tab/impl/ManualPatchTab.kt
+++ manager/src/main/kotlin/me/rhunk/snapenhance/manager/ui/tab/impl/ManualPatchTab.kt
@@ -2,7 +2,7 @@ package me.rhunk.snapenhance.manager.ui.tab.impl
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import me.rhunk.snapenhance.manager.ui.tab.Tab
 import me.rhunk.snapenhance.manager.ui.tab.impl.download.SEDownloadTab
 import me.rhunk.snapenhance.manager.ui.tab.impl.download.SnapchatPatchTab
@@ -10,16 +10,16 @@ import me.rhunk.snapenhance.manager.ui.tab.impl.download.SnapchatPatchTab
 class ManualPatchTab : Tab("manualpatch") {
     override fun init(activity: ComponentActivity) {
         super.init(activity)
-        registerNestedTab(SEDownloadTab::class)
-        registerNestedTab(SnapchatPatchTab::class)
+        // Register tabs if you have nested navigation (optional, not strictly necessary for Compose only)
+        // registerNestedTab(SEDownloadTab::class)
+        // registerNestedTab(SnapchatPatchTab::class)
     }
 
     @Composable
     override fun Content() {
-        // Show both SnapEnhance and Snapchat patch UI in one screen
         Column {
-            navigation.getNestedTab(SEDownloadTab::class)?.Content()
-            navigation.getNestedTab(SnapchatPatchTab::class)?.Content()
+            SEDownloadTab().Content()
+            SnapchatPatchTab().Content()
         }
     }
 }
