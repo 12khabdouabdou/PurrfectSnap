9748cd3d202c8e361e6296e60930ff10b1364b17
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-27 16:56:44 +0530

    Update MainActivity.kt

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/MainActivity.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.background
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
