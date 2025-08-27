1a6efe75bf30e3089f928a6e1f8af5c2c5b4a0e8
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-28 02:18:37 +0530

    Update MainActivity.kt

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/MainActivity.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/MainActivity.kt
@@ -28,6 +28,7 @@ import me.rhunk.snapenhance.SharedContextHolder
 import me.rhunk.snapenhance.common.ui.AppMaterialTheme
 import me.rhunk.snapenhance.common.ui.ThemeMode
 import me.rhunk.snapenhance.common.ui.ThemePreferences
+import androidx.compose.foundation.isSystemInDarkTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
