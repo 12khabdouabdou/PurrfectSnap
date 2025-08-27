d1752e5fe0229549b8f7e1b2307c7d8516db419e
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-08-27 16:45:02 +0530

    Update MappingsScreen.kt

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/setup/screens/impl/MappingsScreen.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/setup/screens/impl/MappingsScreen.kt
@@ -16,24 +16,21 @@ import me.rhunk.snapenhance.ui.util.AlertDialogs
 
 class MappingsScreen : SetupScreen() {
     @Composable
-    override fun Content() {
+    override fun Content() = EdgeToEdge {
         val coroutineScope = rememberCoroutineScope()
         var infoText by remember { mutableStateOf(null as String?) }
         var isGenerating by remember { mutableStateOf(false) }
-
         if (infoText != null) {
             fun dismiss() {
                 infoText = null
                 goNext()
             }
-
             Dialog(onDismissRequest = { dismiss() }) {
                 remember { AlertDialogs(context.translation) }.InfoDialog(title = infoText!!) {
                     dismiss()
                 }
             }
         }
-
         LaunchedEffect(Unit) {
             coroutineScope.launch(Dispatchers.IO) {
                 if (isGenerating) return@launch
@@ -43,7 +40,6 @@ class MappingsScreen : SetupScreen() {
                         throw Exception(context.translation["setup.mappings.generate_failure_no_snapchat"])
                     }
                     val warnings = context.mappings.refresh()
-
                     if (warnings.isNotEmpty()) {
                         isGenerating = false
                         infoText = "${warnings.size} warning(s) occurred while generating mappings:\n\n${warnings.joinToString("\n")}".also {
@@ -51,7 +47,6 @@ class MappingsScreen : SetupScreen() {
                         }
                         return@launch
                     }
-
                     withContext(Dispatchers.Main) {
                         goNext()
                     }
@@ -62,7 +57,6 @@ class MappingsScreen : SetupScreen() {
                 }
             }
         }
-
         if (isGenerating) {
             DialogText(text = context.translation["setup.mappings.dialog"])
             CircularProgressIndicator(
