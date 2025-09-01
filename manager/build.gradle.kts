f72eab5d89166317f040f8f53306c23617b080e1
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-09-02 01:30:12 +0530

    Update build.gradle.kts

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -89,4 +89,7 @@ dependencies {
     implementation(project(":common"))
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.datastore:datastore-core:1.0.0")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.ui:ui-tooling")
 }
