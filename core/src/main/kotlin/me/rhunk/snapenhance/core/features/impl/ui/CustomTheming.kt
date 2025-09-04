c3c48eb467ed0e408a1168ff644d0d41975d1f2d
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-09-04 20:00:33 +0530

    Update CustomTheming.kt

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/ui/CustomTheming.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/ui/CustomTheming.kt
@@ -9,6 +9,8 @@ import me.rhunk.snapenhance.core.util.ktx.getObjectField
 
 class CustomTheming : Feature("Custom Theming") {
     private val amoledBlack = 0xFF000000.toInt()
+    private val onlyPatchId = 0x7f0404b8 // The attrId you want to patch for AMOLED
+
     private val colorTypes = setOf(
         TypedValue.TYPE_INT_COLOR_ARGB8,
         TypedValue.TYPE_INT_COLOR_RGB8,
@@ -16,28 +18,25 @@ class CustomTheming : Feature("Custom Theming") {
         TypedValue.TYPE_INT_COLOR_RGB4
     )
 
-    // All attrIds from 0x7f040000 to 0x7f04009b (inclusive)
-    private val candidateAttrIds = (0x7f040000..0x7f04009b).toList().toTypedArray()
-
     override fun init() {
         if (!context.config.userInterface.forceAmoledTheme.get()) return
 
         onNextActivityCreate {
-            context.androidContext.theme.javaClass.getMethod("obtainStyledAttributes", IntArray::class.java).hook(
-                HookStage.AFTER
-            ) { param ->
-                val array = param.arg<IntArray>(0)
-                val attrId = array[0]
-                val result = param.getResult() as TypedArray
-                val typedArrayData = result.getObjectField("mData") as IntArray
-                val type = result.getType(0)
-                if (type in colorTypes && attrId in candidateAttrIds) {
-                    typedArrayData[1] = amoledBlack
-                    context.log.error(
-                        "[AMOLED PATCH BULK] Patched attrId 0x${attrId.toString(16)} to AMOLED black"
-                    )
+            context.androidContext.theme.javaClass
+                .getMethod("obtainStyledAttributes", IntArray::class.java)
+                .hook(HookStage.AFTER) { param ->
+                    val array = param.arg<IntArray>(0)
+                    val attrId = array[0]
+                    val result = param.getResult() as TypedArray
+                    val typedArrayData = result.getObjectField("mData") as IntArray
+                    val type = result.getType(0)
+                    if (type in colorTypes && attrId == onlyPatchId) {
+                        typedArrayData[1] = amoledBlack
+                        context.log.error(
+                            "[AMOLED PATCH] Patched ONLY attrId 0x${attrId.toString(16)} to AMOLED black"
+                        )
+                    }
                 }
-            }
         }
     }
 }
