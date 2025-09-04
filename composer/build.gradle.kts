c73cc17777c254c9ba59add014baaf5e0c383aae
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-09-05 00:42:54 +0530

    Update build.gradle.kts

--- composer/build.gradle.kts
+++ composer/build.gradle.kts
@@ -1,4 +1,6 @@
 import org.apache.tools.ant.taskdefs.condition.Os
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
@@ -8,10 +10,24 @@ android {
     namespace = rootProject.ext["applicationId"].toString() + ".composer"
     compileSdk = 34
 
+    // Ensure assets folder is wired for the generated bundle
     sourceSets {
         getByName("main") {
             assets.srcDirs("build/assets")
         }
+    }
+
+    // Keep Java toolchain aligned with Kotlin jvmTarget
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+// New Kotlin compilerOptions DSL for Kotlin 2.x (replaces deprecated android.kotlinOptions)
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
@@ -32,7 +48,6 @@ task("compileTypeScript") {
                 commandLine("npx", "--yes", "rollup", "--config", "rollup.config.js", "--bundleConfigAsCjs")
             }
         }
-
         project.copy {
             from("build/loader.js")
             into("build/assets/composer")
