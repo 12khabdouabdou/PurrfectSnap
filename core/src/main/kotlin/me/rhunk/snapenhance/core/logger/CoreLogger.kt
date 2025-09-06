94a38419754830e9ff6db0a80fe6074645d2b0b1
Suryadip Sarkar <137927782+particle-box@users.noreply.github.com>
2025-09-06 07:05:48 +0530

    Update CoreLogger.kt

--- core/src/main/kotlin/me/rhunk/snapenhance/core/logger/CoreLogger.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/logger/CoreLogger.kt
@@ -10,24 +10,22 @@ import me.rhunk.snapenhance.core.bridge.BridgeClient
 import me.rhunk.snapenhance.core.util.hook.HookStage
 import me.rhunk.snapenhance.core.util.hook.hook
 
+
 @SuppressLint("PrivateApi")
 class CoreLogger(
     private val bridgeClient: BridgeClient
-) : AbstractLogger(LogChannel.CORE) {
-
+): AbstractLogger(LogChannel.CORE) {
     companion object {
         private const val TAG = "SnapEnhanceCore"
 
         fun xposedLog(message: Any?, tag: String = TAG) {
-            val text = message?.toString() ?: "null"
-            Log.println(Log.INFO, tag, text)
-            XposedBridge.log("$tag: $text")
+            Log.println(Log.INFO, tag, message.toString())
+            XposedBridge.log("$tag: $message")
         }
 
         fun xposedLog(message: Any?, throwable: Throwable, tag: String = TAG) {
-            val text = message?.toString() ?: "null"
-            Log.println(Log.INFO, tag, text)
-            XposedBridge.log("$tag: $text")
+            Log.println(Log.INFO, tag, message.toString())
+            XposedBridge.log("$tag: $message")
             XposedBridge.log(throwable)
         }
     }
@@ -35,18 +33,11 @@ class CoreLogger(
     private var invokeOriginalPrintLog: (Int, String, String) -> Unit
 
     init {
-        val printLnMethod = Log::class.java.getDeclaredMethod(
-            "println",
-            Int::class.java,
-            String::class.java,
-            String::class.java
-        )
-
-        // Hook without type arguments - the hook function doesn't accept them
+        val printLnMethod = Log::class.java.getDeclaredMethod("println", Int::class.java, String::class.java, String::class.java)
         printLnMethod.hook(HookStage.BEFORE) { param ->
-            val priority = param.args()[0] as Int
-            val tag = param.args()[1] as String
-            val message = param.args()[2] as String
+            val priority = param.arg(0) as Int
+            val tag = param.arg(1) as String
+            val message = param.arg(2) as String
             internalLog(tag, LogLevel.fromPriority(priority) ?: LogLevel.INFO, message)
         }
 
@@ -54,29 +45,33 @@ class CoreLogger(
             XposedBridge.invokeOriginalMethod(
                 printLnMethod,
                 null,
-                arrayOf<Any>(priority, tag, message)
+                arrayOf(priority, tag, message)
             )
         }
     }
 
-    // Normalize to String early to avoid ambiguous T inference at call sites
     private fun internalLog(tag: String, logLevel: LogLevel, message: Any?) {
-        val text = message?.toString() ?: "null"
         runCatching {
-            bridgeClient.broadcastLog(tag, logLevel.shortName, text)
+            bridgeClient.broadcastLog(tag, logLevel.shortName, message.toString())
         }.onFailure {
-            invokeOriginalPrintLog(logLevel.priority, tag, text)
+            invokeOriginalPrintLog(logLevel.priority, tag, message.toString())
         }
     }
 
     override fun debug(message: Any?, tag: String) = internalLog(tag, LogLevel.DEBUG, message)
+
     override fun error(message: Any?, tag: String) = internalLog(tag, LogLevel.ERROR, message)
+
     override fun error(message: Any?, throwable: Throwable, tag: String) {
         internalLog(tag, LogLevel.ERROR, message)
         internalLog(tag, LogLevel.ERROR, throwable.stackTraceToString())
     }
+
     override fun info(message: Any?, tag: String) = internalLog(tag, LogLevel.INFO, message)
+
     override fun verbose(message: Any?, tag: String) = internalLog(tag, LogLevel.VERBOSE, message)
+
     override fun warn(message: Any?, tag: String) = internalLog(tag, LogLevel.WARN, message)
+
     override fun assert(message: Any?, tag: String) = internalLog(tag, LogLevel.ASSERT, message)
 }
