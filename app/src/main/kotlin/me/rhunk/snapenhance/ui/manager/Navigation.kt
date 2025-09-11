53c96051f83bdbc58fdd94e0528ab0374e82cbec
ΞTΞRNAL <suryadip.2008@gmail.com>
2025-09-12 01:23:18 +0530

    FIX

--- app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/Navigation.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/ui/manager/Navigation.kt
@@ -17,7 +17,7 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.border; import androidx.compose.foundation.ExperimentalFoundationApi; import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.FlowRow
