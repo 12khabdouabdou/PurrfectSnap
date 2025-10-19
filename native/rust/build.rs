f935e53b16b6bc1067d3c4919750fd1cd0cd1106
ΞTΞRNAL <suryadip.2008@gmail.com>
2025-10-19 08:39:08 +0530

    fix

--- native/rust/build.rs
+++ native/rust/build.rs
@@ -8,15 +8,16 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("secrets.rs");
     let mut f = File::create(&dest_path).unwrap();
 
-    f.write_all(b"#[no_mangle]\n").unwrap();
-    f.write_all(b"#[allow(non_snake_case)]\n").unwrap();
-    f.write_all(b"pub extern \"C\" fn Java_me_rhunk_snapenhance_core_SecurityFeatures_getSecretKey() -> *const std::os::raw::c_char {\n").unwrap();
-    f.write_all(b"    let secret_key = env!(\"BYPASS_SECRET_KEY\", \"");\n").unwrap();
-    f.write_all(b"    let c_str = std::ffi::CString::new(secret_key).unwrap();\n").unwrap();
-    f.write_all(b"    c_str.into_raw()\n").unwrap();
-    f.write_all(b"}\n").unwrap();
+    f.write_all(br#"
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn Java_me_rhunk_snapenhance_core_SecurityFeatures_getSecretKey() -> *const std::os::raw::c_char {
+    let secret_key = env!("BYPASS_SECRET_KEY", "");
+    let c_str = std::ffi::CString::new(secret_key).unwrap();
+    c_str.into_raw()
+}
+"#).unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=BYPASS_SECRET_KEY");
 }
-
