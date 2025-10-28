cc8cf5c4a1b49b226fe5b8a6d75d81195c0a98cb
12khabdouabdou <zaaaabd7@gmail.com>
2025-10-28 20:08:33 +0100

    Update GalleryVideoSplitting.kt

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/tweaks/GalleryVideoSplitting.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/tweaks/GalleryVideoSplitting.kt
@@ -306,9 +306,9 @@ class GalleryVideoSplitting : Feature("Gallery Video Splitting") {
         val messageDestinations = MessageDestinations(
             AbstractWrapper.newEmptyInstance(context.classCache.messageDestinations)
         ).also {
-            it.conversations = conversations.toCollection(ArrayList<Any>())
-            it.mPhoneNumbers = arrayListOf<Any>()
-            it.stories = arrayListOf<Any>()
+            it.conversations = conversations.toCollection(ArrayList<SnapUUID>())
+            it.mPhoneNumbers = arrayListOf<SnapUUID>()
+            it.stories = arrayListOf<SnapUUID>()
         }
 
         val callback = CallbackBuilder(sendMessageCallback).build()
