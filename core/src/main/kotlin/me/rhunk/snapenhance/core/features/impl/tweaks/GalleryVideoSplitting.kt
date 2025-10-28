01512c419290c1b5485539dd6b6680a10d9567a1
12khabdouabdou <zaaaabd7@gmail.com>
2025-10-28 19:39:59 +0100

    Update GalleryVideoSplitting.kt

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/tweaks/GalleryVideoSplitting.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/tweaks/GalleryVideoSplitting.kt
@@ -312,12 +312,12 @@ class GalleryVideoSplitting : Feature("Gallery Video Splitting") {
         }
 
         val callback = CallbackBuilder(sendMessageCallback)
-            .override("onSuccess") { _ ->
+            .override("onSuccess", callback = { _: HookAdapter ->
                 context.log.verbose("GalleryVideoSplitting: Chunk sent successfully")
-            }
-            .override("onError") { param ->
+            })
+            .override("onError", callback = { param: HookAdapter ->
                 context.log.error("GalleryVideoSplitting: Failed to send chunk: ${param.arg<Any>(0)}")
-            }
+            })
             .build()
 
         val conversationManager = context.feature(Messaging::class).conversationManager?.instanceNonNull()
