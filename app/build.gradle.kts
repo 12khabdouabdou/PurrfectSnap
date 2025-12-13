34a7c27ea99910502f205ede72be41a8226d75d4
12khabdouabdou <zaaaabd7@gmail.com>
2025-12-13 22:47:52 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,7 +121,11 @@ dependencies {
             dependencies.add("${flavorName}Implementation", dependencyNotation)
         }
     }
-
+mplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(project(":core"))
     implementation(project(":common"))
     implementation(libs.androidx.documentfile)
