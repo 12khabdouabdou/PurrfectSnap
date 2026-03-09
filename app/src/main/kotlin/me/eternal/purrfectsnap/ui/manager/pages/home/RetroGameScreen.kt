c52f60a29bedb8f583009d9e6a2fa75f48ed206e
DarkKnight2122 <145188583+DarkKnight2122@users.noreply.github.com>
2026-03-10 04:30:04 +0530

    Introducing Aphelion Theme

--- app/src/main/kotlin/me/eternal/purrfectsnap/ui/manager/pages/home/RetroGameScreen.kt
+++ app/src/main/kotlin/me/eternal/purrfectsnap/ui/manager/pages/home/RetroGameScreen.kt
@@ -155,6 +155,7 @@ class RetroGameScreen : Routes.Route() {
         }
 
         LaunchedEffect(Unit) {
+            context.shortToast(routes.homeAbout.translation["about_magic_toast"])
             resetGame()
             while (true) {
                 delay(16)
